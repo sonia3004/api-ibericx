fffb043f42fcd187eea2a767670d91334a5c6af2
Sonia boutaleb <sonia.boutaleb@next-u.fr>
2024-10-09 14:32:26 +0200

    Mise à jour du fichier products.js

--- models/produit.js
+++ models/produit.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const produitSchema = new mongoose.Schema({
-  id: { type: Number, required: true, unique: true },
   name: { type: String, required: true },
   price: { type: String, required: true },
   href: { type: String, required: true },
@@ -14,6 +13,28 @@ const produitSchema = new mongoose.Schema({
   sold: { type: Number, default: 0 },
   quantity: { type: Number, required: true },
   description: { type: String, required: true },
+  category: { type: String, required: true }, // Ajout du champ category
+  size: {
+    36: { type: Boolean, default: false },
+    37: { type: Boolean, default: false },
+    38: { type: Boolean, default: false },
+    39: { type: Boolean, default: false },
+    40: { type: Boolean, default: false },
+    41: { type: Boolean, default: false },
+    42: { type: Boolean, default: false },
+    43: { type: Boolean, default: false },
+    44: { type: Boolean, default: false },
+    45: { type: Boolean, default: false },
+    46: { type: Boolean, default: false },
+  },
+});
+
+// Ajout d'une méthode pour inclure les tailles uniquement si le produit est une chaussure
+produitSchema.pre('save', function (next) {
+  if (this.category !== 'chaussures') {
+    this.size = undefined; // Supprime les tailles si ce n'est pas une chaussure
+  }
+  next();
 });
 
 const Produit = mongoose.model('Produit', produitSchema);
